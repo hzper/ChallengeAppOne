847f498a1214f97f29d25178c75840dc05f7d103
hzper <hzpper@gmail.com>
2022-06-22 23:22:05 +0200

    Initial Commit

---
+++ ChallengeAppOne/ChallengeAppOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeAppOneApp.swift
+//  ChallengeAppOne
+//
+//  Created by David Dolački on 22.06.2022..
+//
+
+import SwiftUI
+
+@main
+struct ChallengeAppOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
